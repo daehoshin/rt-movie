d92a3f96d4a244b4e91b72141fffc6bc2ceb25a6
daehoshin <daehoshin87@gmail.com>
2020-01-29 17:47:12 +0900

    로딩바 추가

--- src/App.js
+++ src/App.js
@@ -44,7 +44,7 @@ class App extends  Component {
       const {movies} = this.state;
       return (
           <div className={movies ? "App" : "App--loading"}>
-              {movies ? this._renderMovies() : 'Loading'}
+              {movies ? this._renderMovies() : "" }
           </div>
       );
     }
